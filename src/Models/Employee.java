4e147b0c532b7a455c4bcf5515d5673d911744af
Fabian Aguirre <aguirrefabian.unne@gmail.com>
2022-05-26 13:52:03 -0300

    Modify logic of the class

---
+++ src/Models/Employee.java
@@ -0,0 +1,36 @@
+package Models;
+
+import java.util.Date;
+
+/**
+ * <h2>Employee</h2>
+ * {@code Employee} is a subclass<br>
+ * <p>
+ * This class belong to the group User of system.<br>
+ * This class extends of {@User} *
+ * <p>
+ * @author AguirreFab
+ * @version 1.0
+ *
+ */
+
+public class Employee extends User{
+    private Integer personalFile;
+
+
+    public Employee(String cuil, String name, String surname, String address, String phoneNumber, String email, Date registrationDate, Integer branchId, Integer personalFile) {
+        super(cuil, name, surname, address, phoneNumber, email, registrationDate, branchId);
+        this.personalFile = personalFile;
+    }
+
+    public Integer getPersonalFile() {
+        return personalFile;
+    }
+
+    public void setPersonalFile(Integer personalFile) {
+        this.personalFile = personalFile;
+    }
+}
+
+
+
---
+++ src/Models/User.java
@@ -0,0 +1,102 @@
+package Models;
+
+import java.util.Date;
+
+/**
+ * <h2>Usuario</h2>
+ * {@code Usuario} es una clase abstracta <br>
+ * <p>
+ * Es una superclase correspondiente a la familia de usuarios del sistema. <br>
+ * No es posible crear instancias de esta clase. *
+ * <p>
+ *
+ * @author AguirreFab
+ * @version 1.0
+ */
+
+public abstract class User {
+    protected String cuil;
+    protected String name;
+    protected String surname;
+    protected String address;
+    protected String phoneNumber;
+    protected String email;
+    protected Date registrationDate;
+    protected Integer branchId;
+
+    public User(String cuil, String name, String surname, String address, String phoneNumber, String email, Date registrationDate, Integer branchId) {
+        this.cuil = cuil;
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.registrationDate = registrationDate;
+        this.branchId = branchId;
+    }
+
+    public String getCuil() {
+        return cuil;
+    }
+
+    public void setCuil(String cuil) {
+        this.cuil = cuil;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public Integer getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(Integer branchId) {
+        this.branchId = branchId;
+    }
+}
+
