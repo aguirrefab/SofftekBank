c5b448535b57e3d189ae8c5133cbb3422f93fa0e
Fabian Aguirre <aguirrefabian.unne@gmail.com>
2022-05-27 10:05:16 -0300

    add Interface for bank services

--- src/Services/IBankService.java
+++ src/Services/IBankService.java
@@ -1,2 +1,17 @@
-package Services;public interface IBankService {
+package Services;
+
+/**
+ * <h2>Bank Service</h2>
+ * {@code IBankService} is an interface<br>
+ * <p>
+ * This interface defines the methods for commons behaviors.<br>
+ * <p>
+ * @author AguirreFab
+ * @version 1.0
+ *
+ */
+
+public interface IBankService {
+    
+
 }
