89e72de9f9c71f77f1cb6078a00f154c3a24cb2a
Fabian Aguirre <aguirrefabian.unne@gmail.com>
2022-05-27 10:09:44 -0300

    Create the first enum object for define Types of Accounts

--- src/Constants/TypeOfAccount.java
+++ src/Constants/TypeOfAccount.java
@@ -1,2 +1,19 @@
-package Constants;public class TypeOfAccount {
+package Constants;
+
+public enum TypeOfAccount {
+
+    SAVING_ACCOUNT(100, "Saving Account"),
+    CHECKING_ACCOUNT(200, "Checking Account");
+
+    private Integer accountCode;
+    private String descriptionAccount;
+
+    TypeOfAccount(Integer accountCode, String descriptionAccount) {
+        this.accountCode = accountCode;
+        this.descriptionAccount = descriptionAccount;
+    }
+
+
+
+
 }
