d3a9bf32e754bd584cd3d3cf5a35983f396353c1
Fabian Aguirre <aguirrefabian.unne@gmail.com>
2022-05-29 21:40:53 -0300

    Add the main class for the test system

--- src/Main/SofttekSystemBank.java
+++ src/Main/SofttekSystemBank.java
@@ -1,2 +1,18 @@
-package Main;public class SofttekSystemBank {
+package Main;
+
+import Constants.TypeOfAccount;
+import Models.Accounts.SavingAccount;
+import Models.Users.AdministratorSystem;
+
+public class SofttekSystemBank {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
+
 }
