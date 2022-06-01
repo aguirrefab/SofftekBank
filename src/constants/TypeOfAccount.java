9fb878ebe67395288a5301f994b29cf733156dfb
Fabian Aguirre <aguirrefabian.unne@gmail.com>
2022-06-01 07:19:02 -0300

    modify the enums class

--- src/constants/TypeOfAccount.java
+++ src/constants/TypeOfAccount.java
@@ -2,8 +2,8 @@ package constants;
 
 public enum TypeOfAccount {
 
-    SAVING_ACCOUNT(100, "Saving Account"),
-    CHECKING_ACCOUNT(200, "Checking Account");
+    SAVING_ACCOUNT(101, "Saving Account"),
+    CHECKING_ACCOUNT(202, "Checking Account");
 
     private Integer accountCode;
     private String descriptionAccount;
--- src/constants/TypeOfCurrency.java
+++ src/constants/TypeOfCurrency.java
@@ -1,2 +1,15 @@
-package constants;public enum TypeOfCurrency {
+package constants;
+
+public enum TypeOfCurrency {
+
+    DOLLAR("USD", "United States dollar"),
+    PESO_AR("ARS","Argentine peso");
+
+    private String code;
+    private String description;
+
+    TypeOfCurrency(String code, String description) {
+        this.code = code;
+        this.description = description;
+    }
 }
